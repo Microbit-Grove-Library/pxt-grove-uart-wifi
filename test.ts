976a920009d6af092afb046d98ac7270205fe117
jerryyip <chdyejiewei@gmail.com>
2020-09-10 13:57:55 +0800

    feat: add IFTTT

--- test.ts
+++ test.ts
@@ -8,10 +8,11 @@ groveuartwifi.setupWifi(
 
 basic.forever(() => {
     if (groveuartwifi.wifiOK()) {
-        basic.showNumber(1);
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
     }
-    else {
-        basic.showNumber(0);
-    }
-    basic.pause(1000);
+    groveuartwifi.sendToThinkSpeak("write_api_key", 1, 2, 3, 4, 5, 6, 7, 8)
+    groveuartwifi.sendToIFTTT("ifttt_event", "ifttt_key", "hello", 'micro', 'bit')
+    basic.pause(60000)
 })
